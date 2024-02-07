a67a35c2401c2c1fd73d05145f72974f8e23c25a
Waji-97 <wajiwos16@gmail.com>
2024-02-07 23:31:48 +0900

    'test1'

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from myapp import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', include('myapp.urls')),
 ]
